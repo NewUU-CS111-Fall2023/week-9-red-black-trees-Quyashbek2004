01b772282cae4ae5bdc77ca28d11280f66d59f5b
Quyashbek2004 <127374399+Quyashbek2004@users.noreply.github.com>
2023-12-13 19:36:19 +0500

    Create task_5.h

---
+++ src/task_5.h
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+enum Color { RED, BLACK };
+
+struct Node {
+    int data;
+    Color color;
+    Node* left, *right, *parent;
+
+    Node(int data) : data(data), color(RED), left(nullptr), right(nullptr), parent(nullptr) {}
+};
+
+class RedBlackTree {
+private:
+    Node* root;
+
+    // ... (other methods and declarations)
+
+    int countChildren(Node* node) {
+        int count = 0;
+        if (node->left != nullptr) count++;
+        if (node->right != nullptr) count++;
+        return count;
+    }
+
+    void countChildrenUtil(Node* node) {
+        if (node == nullptr) return;
+        int childrenCount = countChildren(node);
+        cout << node->data << " - " << childrenCount << endl;
+        countChildrenUtil(node->left);
+        countChildrenUtil(node->right);
+    }
+
+public:
+    RedBlackTree() : root(nullptr) {}
+
+    // ... (other methods like insert, printTree, etc.)
+
+    void countChildrenForEachNode() {
+        countChildrenUtil(root);
+    }
+};
+
+int main() {
+    int N, value;
+    cin >> N;
+    RedBlackTree tree;
+
+    for (int i = 0; i < N; ++i) {
+        cin >> value;
+        tree.insert(value);
+    }
+
+    tree.countChildrenForEachNode();
+
+    return 0;
+}
+//O(n)
